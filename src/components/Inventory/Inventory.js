ddd0590bee1624b5c72c23836e4bfa36d11a9b73
AyuburRahman <aiburrahman17@gmail.com>
2021-09-30 22:59:37 +0600

    updated'

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is Inventory</h2>
+            <h2>This is Inventory page and change with git own repo</h2>
         </div>
     );
 };
